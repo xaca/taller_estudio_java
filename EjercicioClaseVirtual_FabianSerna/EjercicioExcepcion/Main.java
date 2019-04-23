a0bb689ac6e2f04d0fd13ea868bc9db2cc91fdf2
Fabian <fabianserna2013@gmail.com>
2019-04-23 16:06:58 -0500

    Excepcion personalizado

--- EjercicioClaseVirtual_FabianSerna/EjercicioExcepcion/Main.java
+++ EjercicioClaseVirtual_FabianSerna/EjercicioExcepcion/Main.java
@@ -1,25 +1,19 @@
-import java.io.*;
-
 public class Main {
     public static void main(String [ ] args) {
         
         // Una cadena mayor o igual a 4 caracteres
         String cadena = "hol";
         try{
-            VerificarCadena(cadena);
-        }catch(EOFException e){
-            System.out.println("La cadena es menor a 4 caracteres");
+            
+            if( cadena.length() >= 4 ){
+                System.out.println("Genial");
+            }else{
+                throw new CaracteresExcepcion("La cadena es menor a 4 caracteres");
+            }
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
-    }
-
-    public static void VerificarCadena(String cadena) throws EOFException {
-
-        if( cadena.length() < 4 ){
-            throw new EOFException();
-        }else{
-            System.out.println("Genial");
-        }
-
     }
 
 }
