76d9e42656f73092cfcfee2c9747cd5dc75c0613
izuluagag1998 <izuluagag1998@gmail.com>
2019-04-23 17:52:14 -0500

    Mejora al eje excepcion

--- actividadDiaSinCarro/ejeInterfaz/Operaciones.java
+++ actividadDiaSinCarro/ejeInterfaz/Operaciones.java
@@ -1,6 +1,6 @@
 public interface Operaciones {
    
     int sumarNumeros(); //Suma los numeros entre los limites establecidos
-    void imprimirNumeros(); // Resta los numeros entre los limites establecidos 
-    void imprimirNumerosPares(); // multiplica los numeros entre los limites establecidos
+    void imprimirNumeros(); 
+    void imprimirNumerosPares(); 
 }
